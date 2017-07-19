920964af77448b863d6ae8730753fc98ee023b39
Bertuğ <ylmazbertug@gmail.com>
2017-07-19 13:14:46 +0300

    init Project

---
+++ Custom-Chat/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Custom-Chat
+//
+//  Created by Bertuğ YILMAZ on 19/07/2017.
+//  Copyright © 2017 bertug. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
