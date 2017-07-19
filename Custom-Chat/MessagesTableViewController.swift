4d86f71f65da86d3b6cc3d0c766e673c75ffe818
Bertuğ <ylmazbertug@gmail.com>
2017-07-19 13:26:41 +0300

    Added MessagesTableViewController

---
+++ Custom-Chat/MessagesTableViewController.swift
@@ -0,0 +1,38 @@
+//
+//  MessagesTableViewController.swift
+//  Custom-Chat
+//
+//  Created by Bertuğ YILMAZ on 19/07/2017.
+//  Copyright © 2017 bertug. All rights reserved.
+//
+
+import UIKit
+
+class MessagesTableViewController: UITableViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+    }
+
+    // MARK: - Table view data source
+
+    override func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    }
+
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 1
+    }
+
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
+
+        // Configure the cell...
+
+        return cell
+    }
+}
