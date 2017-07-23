94182356c61971b91370183646fd039058a10347
Bertuğ <ylmazbertug@gmail.com>
2017-07-23 17:25:25 +0300

    Added receiver cell #1

--- Custom-Chat/ChatViewController.swift
+++ Custom-Chat/ChatViewController.swift
@@ -12,6 +12,8 @@ class ChatViewController: UIViewController {
 
     @IBOutlet weak var bottomView: UIView!     // View was used instead of toolbar because it is more useful :]
     @IBOutlet weak var tableView: UITableView!
+    
+    var thatsIam : Bool = true
   
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -47,21 +49,34 @@ class ChatViewController: UIViewController {
 extension ChatViewController : UITableViewDelegate, UITableViewDataSource {
     
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int{
-        return 1
+        return 2
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell{
-        let cell = tableView.dequeueReusableCell(withIdentifier: "senderCell") as! SenderTableViewCell
-        cell.senderImageView.layer.cornerRadius = 15
-        cell.senderMessageTextView.layer.cornerRadius = 10
-        
-        cell.senderImageView.image = UIImage(named: "user")
-        cell.senderMessageTextView.text = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut bibendum eleifend dui ac tristique. Donec tincidunt luctus facilisis. Nulla libero eros, iaculis nec gravida quis, luctus a mauris. Morbi pharetra mauris eget cursus dapibus. Morbi rutrum facilisis metus, ac volutpat libero tristique ut. Phasellus egestas lectus nec massa blandit, nec tempus nunc fermentum. Nulla facilisi. Vestibulum pulvinar, mi id semper porttitor, metus lectus fringilla mauris, vel molestie nisl lectus eget arcu. Aenean eget nisl ultricies, mollis magna et, lobortis nisi. Donec in placerat elit, at laoreet justo. Donec pretium mauris sapien, bibendum feugiat erat sagittis quis. Ut magna neque, vulputate non iaculis a, efficitur et nunc."
-        cell.lastTimeLabel.text = "12.12.1212"
-        return cell
-    }
-    
-    
 
+        self.thatsIam = indexPath.row == 1 ? false : true
+        
+        if self.thatsIam {
+            let cell = tableView.dequeueReusableCell(withIdentifier: "receiverCell", for: indexPath) as! ReceiverTableViewCell
+            
+            cell.receiverImageView.layer.cornerRadius = 15
+            cell.receiverMessageTextView.layer.cornerRadius = 10
+            
+            cell.receiverImageView.image = UIImage(named: "user")
+            cell.receiverMessageTextView.text = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut bibendum eleifend dui ac tristique. Donec tincidunt luctus facilisis. Nulla libero eros, iaculis nec gravida quis, luctus a mauris. Morbi pharetra mauris eget cursus dapibus. Morbi rutrum facilisis metus, ac volutpat libero tristique ut. Phasellus egestas lectus nec massa blandit, nec tempus nunc fermentum. Nulla facilisi. Vestibulum pulvinar, mi id semper porttitor, metus lectus fringilla mauris, vel molestie nisl lectus eget arcu. Aenean eget nisl ultricies, mollis magna et, lobortis nisi. Donec in placerat elit, at laoreet justo. Donec pretium mauris sapien, bibendum feugiat erat sagittis quis. Ut magna neque, vulputate non iaculis a, efficitur et nunc."
+            cell.lastTimeLabel.text = "12.12.1212"
+            
+            return cell
+        }else {
+            let cell = tableView.dequeueReusableCell(withIdentifier: "senderCell") as! SenderTableViewCell
+            cell.senderImageView.layer.cornerRadius = 15
+            cell.senderMessageTextView.layer.cornerRadius = 10
+            
+            cell.senderImageView.image = UIImage(named: "user")
+            cell.senderMessageTextView.text = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut bibendum eleifend dui ac tristique. Donec tincidunt luctus facilisis. Nulla libero eros, iaculis nec gravida quis, luctus a mauris. Morbi pharetra mauris eget cursus dapibus. Morbi rutrum facilisis metus, ac volutpat libero tristique ut. Phasellus egestas lectus nec massa blandit, nec tempus nunc fermentum. Nulla facilisi. Vestibulum pulvinar, mi id semper porttitor, metus lectus fringilla mauris, vel molestie nisl lectus eget arcu. Aenean eget nisl ultricies, mollis magna et, lobortis nisi. Donec in placerat elit, at laoreet justo. Donec pretium mauris sapien, bibendum feugiat erat sagittis quis. Ut magna neque, vulputate non iaculis a, efficitur et nunc."
+            cell.lastTimeLabel.text = "12.12.1212"
+            return cell
+        }
+    }
 }
 
---
+++ Custom-Chat/ReceiverTableViewCell.swift
@@ -0,0 +1,17 @@
+//
+//  ReceiverTableViewCell.swift
+//  Custom-Chat
+//
+//  Created by Bertuğ YILMAZ on 23/07/2017.
+//  Copyright © 2017 bertug. All rights reserved.
+//
+
+import UIKit
+
+class ReceiverTableViewCell: UITableViewCell {
+
+    @IBOutlet weak var receiverMessageTextView: UITextView!
+    @IBOutlet weak var lastTimeLabel: UILabel!
+    @IBOutlet weak var receiverImageView: UIImageView!
+
+}
